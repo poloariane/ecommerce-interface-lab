0d4b23084eefe46f52f944bdb32cf886ca8d5b5d
POLOARIANE <ariane>
2026-03-13 12:23:37 +0800

    Initial commit: create solution.js

---
+++ solution.js
@@ -0,0 +1,27 @@
+function checkVariable(input){
+
+switch(typeof input){
+
+case "string":
+return "string"
+
+case "number":
+return "number"
+
+case "boolean":
+return "boolean"
+
+case "bigint":
+return "bigint"
+
+case "undefined":
+return "undefined"
+
+case "object":
+return "object"
+
+}
+
+}
+
+console.log(checkVariable("hello"))
