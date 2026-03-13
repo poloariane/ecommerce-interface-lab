c4338be2bafc75ed8c509fa27208ac53449d3e7e
POLOARIANE <ariane>
2026-03-13 12:50:44 +0800

    Problem 2: Secure ID Generator

--- solution.js
+++ solution.js
@@ -17,3 +17,18 @@ function checkVariable(input) {
             return "unknown";
     }
 }
+
+// Problem 2: Secure ID Generator
+function generateIDs(count) {
+    const ids = [];
+
+    for (let i = 0; i < count; i++) {
+        if (i === 5) {
+            continue; // skip 5
+        }
+
+        ids.push(`ID-${i}`);
+    }
+
+    return ids;
+}
