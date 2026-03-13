12b921bcad6f815e2065875b287b6cf6447fbe7d
POLOARIANE <ariane>
2026-03-13 13:03:35 +0800

    Problem 5: The Private Inventory

--- solution.js
+++ solution.js
@@ -52,3 +52,17 @@ function getTopScorers(playerList) {
         .map(player => player.name)
         .join(", ");
 }
+// Problem 5: The Private Inventory
+class Item {
+
+    #discount = 0.1;
+
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    get finalPrice() {
+        return this.price - (this.price * this.#discount);
+    }
+}
